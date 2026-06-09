b2ca50aaf754cac1816dce177a257e6b25b993d4
Tien Nguyen <tienlucky2803@gmail.com>
2026-06-09 11:24:38 +0700

    feat: add emdding and answer function

--- src/models/llm_manager.py
+++ src/models/llm_manager.py
@@ -1,26 +1,54 @@
 import ollama
+from src.pipeline.rag_pipeline import RAGPipeline 
 
-PROMPT_TEMPLATE = """Bạn là trợ lý hỏi đáp.
-Dùng các đoạn ngữ cảnh dưới đây để trả lời câu hỏi.
-Nếu ngữ cảnh không có thông tin, hãy nói là bạn không biết, đừng bịa.
-Trả lời ngắn gọn, chính xác, bằng tiếng Việt.
+PROMPT_TEMPLATE = """Bạn là một trợ lý hỏi đáp tài liệu học tập thông minh và trung thực.
+Hãy sử dụng các đoạn ngữ cảnh (Context) được cung cấp dưới đây để trả lời câu hỏi (Question).
 
-Ngữ cảnh:
+---
+NGỮ CẢNH (CONTEXT):
 {context}
+---
 
-Câu hỏi: {question}
-Trả lời:"""
+CÂU HỎI (QUESTION):
+{question}
+
+---
+YÊU CẦU:
+- Trả lời ngắn gọn, rõ ràng, đi thẳng vào vấn đề.
+- Chỉ dựa vào thông tin có trong phần NGỮ CẢNH phía trên.
+- Nếu thông tin trong NGỮ CẢNH không có hoặc không đủ để trả lời, hãy nói thẳng là "Tôi không biết câu trả lời dựa trên tài liệu đã cung cấp", TUYỆT ĐỐI KHÔNG ĐƯỢC BỊA ĐẶT thông tin.
+
+TRẢ LỜI:"""
 
 def get_embedding(texts: list, model_name: str = "bge-m3") -> list:
     """
     Gọi API Ollama để chuyển đổi danh sách văn bản thành danh sách vector.
+    
+    Args: 
+        text (List): List văn bản cần vector hóa
+        model_name (str): Tên mô hình embedding chạy trên Ollama (mặc định: "bge-m3")
+        
+    Return:
+        List: list vector
     """
-    return ollama.embed(model=model_name, input=texts)["embeddings"]
+    embeddings = [] 
+    for text in texts:
+        respone =  ollama.embeddings(
+            model = model_name,
+            promt = text
+        )
+        embeddings.append(respone["embedding"])
+    
+    return embeddings
 
 def generate_answer(question: str, context: str, model_name: str = "vicuna:7b-v1.5-q5_1", temperature: float = 0.0) -> str:
     """
     Gửi prompt chứa ngữ cảnh và câu hỏi đến LLM để nhận câu trả lời.
     """
+    
+    rag = RAGPipeline()
+    context = "\n---\n".join(rag.retrieve(question))
+    
     formatted_prompt = PROMPT_TEMPLATE.format(context=context, question=question)
     resp = ollama.chat(
         model=model_name,
